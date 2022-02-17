7821dcafa4266ce30d0ff077f17246afd7fff35f
Your Name <emiliaodoh1@gmail.com>
2022-02-17 13:08:29 +0100

    state checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import img from './assets/statespic.jpeg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ class App extends Component {
+   state = {
+     fullName: 'Emilia Odoh',
+     bio: 'emilia is a fine girl',
+     imgSrc: img,
+     profession: 'software developer',
+     show: true
+   }
+handleClick= () =>{this.setState({show:!this.state.show})}
+  render() {
+    return (
+      <div>
+        {this.state.show?(<div>
+       <h1>fullName = {this.state.fullName}</h1>
+        <h1>bio = {this.state.bio}</h1>
+        <h1> profession = {this.state.profession}</h1>
+        <img src= { this.state.imgSrc} alt = "."/></div>):""}
+
+        <button onClick={this.handleClick}>Click Here!</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
